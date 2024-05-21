561ab3fc7d65bb2948c2a56ae8c9d51c3a12deb7
Nicolas Freitas <nicolasfpdev@gmail.com>
2024-05-21 16:56:04 -0300

    feat(parser): test consume api movie

--- js/script.js
+++ js/script.js
@@ -1,24 +1,35 @@
 const cardContainer = document.querySelector(".card-container");
 
-const movies = [
-    {
+const url = `https://api.themoviedb.org/3/trending/movie/week?api_key=47d6dc10e42efa4b24ea80a03fe59ca9&language=pt-br`;
 
-    },
-]
+async function apiResult() {
+    const fetchResponse = await fetch(url)
+        .then((response) => response.json())
+        .catch((error) => console.error(error));
+    
+    return fetchResponse;
+}
+
+async function getPopularMovies(){
+    const movies = apiResult();
+    movies.forEach((movie) => {
+        renderMovies(movie);
+    });
+}
 
 function renderMovies(movie) {
-    const { title, image, rating, year, description, isFavorited } = movie
+    const { id, title, poster_path, vote_average, release_date, overview } = movie;
 
     cardContainer.innerHTML = `
     <div class="img-container">
-        <img src=${image} alt="icone do filme">
+        <img src=${poster_path} alt="icone do filme">
     </div>
     <div class="container-movie">
-        <h2 class="movie-title">${title} (${year})</h2>
+        <h2 class="movie-title">${title} (${release_date})</h2>
         <div class="movie-icons">
             <div class="icons-container">
                 <i class="fa-solid fa-star"></i>
-                <span>${rating}</span>
+                <span>${vote_average}</span>
             </div>
             <div class="icons-container">
                 <i class="fa-regular fa-heart"></i>
@@ -27,9 +38,7 @@ function renderMovies(movie) {
         </div>
     </div>
     <div class="container-movie-desc">
-        ${description}
+        ${overview}
     </div>
 `;
 }
-
-
